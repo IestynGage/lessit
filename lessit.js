5e08a4bba28302ccd98c0efdbd3d583d33ef608d
Iestyn <>
2023-02-11 18:41:31 +0000

    More work on extension

--- lessit.js
+++ lessit.js
@@ -15,7 +15,7 @@ const createTextElement = (text) => {
   textElement.appendChild(textNode)
   return textElement;
 }
-
+console.log("Hello world asdasdasdasdas")
 const enterMessage = createTextElement("Please enter the following URL to unlock the page");
 const url = createTextElement(found);
 
@@ -26,13 +26,13 @@ function myFunction(event) {
   document.body.style.overflow = defaultOverflow;
 }
 const form = document.createElement("form");
-form.setAttribute("onsubmit", "myFunction()");
 form.addEventListener('submit', myFunction)
 
 const textInput = document.createElement("input");
 textInput.setAttribute("type", "text");
 textInput.setAttribute("name", "FullName");
 textInput.setAttribute("placeholder", "Enter the above URL");
+textInput.setAttribute("autocomplete", "off");
 
 const submitElement = document.createElement("input");
 submitElement.setAttribute("type", "submit");
@@ -43,8 +43,34 @@ form.appendChild(submitElement);
 
 modal.appendChild(enterMessage);
 modal.appendChild(url);
-modal.appendChild(form)
-const currentDiv = document.getElementById("div1");
-document.body.insertBefore(mask, currentDiv);
 
-document.body.style.overflow = "hidden";
+browser.storage.local.get("reddit")
+  .then(x => {
+    const countText = createTextElement(`You've visited this site ${x.reddit.count}`);
+    modal.appendChild(countText);
+    modal.appendChild(form)
+    const currentDiv = document.getElementById("div1");
+    document.body.insertBefore(mask, currentDiv);
+    window.scrollTo(0,0);
+    document.body.style.overflow = "hidden";
+  });
+
+function increaseSiteCount (site, count) {
+  console.log("here", count)
+  browser.storage.local.set({
+    reddit: {date:"08/02/2023", count: count + 1, limit: 100},
+});
+}
+
+browser.storage.local.get("reddit")
+  .then(x => {
+    const count = Object.keys(x).length === 0 ? 0 : x.reddit.count;
+    increaseSiteCount("reddit", count);
+    console.log("x.count", x.count)
+  }).then(() => {
+    browser.storage.local.get("reddit")
+      .then(y => {
+        console.log("secound get", y)
+      });
+  })
+
